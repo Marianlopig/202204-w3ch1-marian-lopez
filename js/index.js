255de076819a9611cf629f8968ddfb83c4296741
Marian <marianlopig@gmail.com>
2022-04-18 20:06:25 +0200

    all classes created and links between them done

---
+++ js/index.js
@@ -0,0 +1,4 @@
+import Luchador from "./Components/Luchador.js";
+import Rey from "./Components/Rey.js";
+import Asesor from "./Components/Asesor.js"
+import Escudero from "./Components/Escudero.js"
